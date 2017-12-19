09b7fc00aea14bf01c43484d6b7bfa898a2677a7
wsl <395548460@qq.com>
2017-12-19 09:41:46 +0800

    feat : first

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * 小程序配置文件
+ */
+
+// 此处主机域名修改成腾讯云解决方案分配的域名
+var host = 'http://www.dianjiangla.com';
+
+var config = {
+
+    // 下面的地址配合云端 Demo 工作
+    service: {
+        host,
+        // 数据接口 api
+        apiUrl: `${host}/api`
+    }
+};
+
+module.exports = config;
---
+++ utils/api.js
@@ -0,0 +1,58 @@
+var config = require('../config')
+
+/**
+ * 组装接口完整的路径名称
+ * @param {接口名称 /app/index/recommend} urlName 
+ */
+var apiName = function (urlName = '') {
+    return config.service.apiUrl + urlName || '';
+}
+
+/**
+ * 接口调用
+ * @param {接口路径} url
+ * @param {接口的参数} data
+ */
+var api = {
+    cookie: '',
+    setCookie: (respone) => {
+        if (respone.header['Set-Cookie']) {
+            api.cookie = respone.header['Set-Cookie'];
+        }
+    },
+    request: function () {
+        var method = arguments[0];
+        var url = arguments[1];
+        var data = arguments[2];
+
+        return new Promise(function (resolve, reject) {
+            wx.request({
+                url: apiName(url),
+                method: method.toUpperCase(),
+                data: data,
+                dataType: "json",
+                header: {
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'Cookie': api.cookie || ''
+                },
+                success: function (e) {
+                    api.setCookie(e);
+                    resolve(e.data);
+                },
+                fail: function(e){
+                    reject(e.data);
+                }
+            })
+        });
+    }
+}
+
+let like = ['delete', 'get', 'head', 'options', 'post', 'put', 'patch'];
+
+like.forEach(method => {
+    api[method] = function () {
+        return api.request(method, ...arguments);
+    };
+});
+
+module.exports = api
