c437cabbec970e64a9bd0569d7e4d679e32169ee
2355456062@qq.com <2355456062@qq.com>
2018-08-27 13:59:47 +0800

    feat:四喜E伙伴  客户详情

---
+++ pages/customer/customer.js
@@ -0,0 +1,20 @@
+// pages/customer/customer.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  toCustomerDetail:function (){
+    wx.navigateTo({
+      url: "./info/info"
+    })
+  }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+
+})
---
+++ pages/customer/info/info.js
@@ -0,0 +1,21 @@
+// pages/customer/customerDetail/customerDetail.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info:{
+      name:'李东强',
+      nameIcon:'李',
+      rank:'高级会员',
+      identity:'经销商'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  
+})
